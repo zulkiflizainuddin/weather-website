9c73794fd7948eb8339c7e2b49f88ad57b0100c0
unknown <zulkifli10101@gmail.com>
2022-04-27 13:59:36 +0800

    Add humidity to forecast

--- src/ultis/forecast.js
+++ src/ultis/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Please try another search', undefined)
         } else (
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out but it feels like ${body.current.feelslike} degrees`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out but it feels like ${body.current.feelslike} degrees out and the humidity is ${body.current.humidity}%`)
         )
     })
 }
